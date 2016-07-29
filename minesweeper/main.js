8525d0bcae6ad5ac7c5ee3746e4d4e107ac82b72
Jacky Chan <jchan@uber.com>
2016-07-29 00:19:36 -0700

    Refactor into MVC

--- minesweeper/main.js
+++ minesweeper/main.js
@@ -1,182 +1,197 @@
-var field = makeMinesField(7, 7, 10);
-var rootElement = document.querySelector('#root');
-
-renderMinesField();
-
-function renderMinesField() {
-  rootElement.innerHTML = '';
-
-  field.forEach(function(row) {
-    var rowElement = document.createElement('div');
-    rowElement.className = 'minesweeper-row';
-    row.forEach(function(cell) {
-      var cellElement = document.createElement('button');
-      var cellType = cell.isMine ? 'M' : cell.value;
-
-      if (cell.isFlagged) {
-        cellElement.className = 'minesweeper-cell__flagged minesweeper-cell';
-      } else if (cell.isOpen) {
-        cellElement.textContent = cell.isMine || cell.value < 1 ? '' : cell.value;
-        cellElement.className = 'minesweeper-cell minesweeper-cell__clicked minesweeper-cell' + (cellType ? '--' + cellType : '');
-      } else {
-        cellElement.className = 'minesweeper-cell__closed minesweeper-cell';
-      }
-
-      cellElement.addEventListener('click', function(e) {
-        if (cell.isFlagged || cell.isOpen) {
-          return;
-        }
-
-        spread(cell);
-        renderMinesField();
-
-        if (cell.isMine) {
-          console.log('you lose');
-          cell.isOpen = true;
-          renderMinesField();
-        }
-      });
-
-      cellElement.addEventListener('contextmenu', function(e) {
-          if (cell.isOpen) {
-            return;
-          }
-          cell.isFlagged = !cell.isFlagged;
-          e.preventDefault();
-          cellElement.className = cell.isFlagged && 'minesweeper-cell__flagged  minesweeper-cell';
-          renderMinesField();
-      });
-
-      rowElement.appendChild(cellElement);
-    });
-
-    rootElement.appendChild(rowElement);
-  });
-}
-
-function spread(cell) {
-  if (!cell || cell.isMine || cell.isOpen) {
-    return;
+/**
+  Minesweeper Model Class
+*/
+class MinesweeperModel {
+  constructor(numOfRows, numOfCols, numOfMines) {
+    const length = numOfRows * numOfCols;
+    this.row = numOfRows;
+    this.col = numOfCols;
+    this.mines = numOfMines;
+    this.field = this.makeField(numOfRows, numOfCols, numOfMines);
+    this.isOpen = Array(length).fill(false);
+    this.isFlag = Array(length).fill(false);
   }
 
-  cell.isOpen = true;
-
-  if (cell.value >= 1) {
-    return;
+  makeField(numOfRows, numOfCols, numOfMines) {
+    let field = this.createFieldWithMines(numOfRows, numOfCols, numOfMines);
+    field = this.addNumberIndicatorsToMines(field);
+    return field;
   }
 
-  var row = cell.row;
-  var col = cell.col;
-  var east, west, north, south, northeast, northwest, southeast, southwest;
-
-  if (field[row - 1]) {
-    north = field[row - 1][col];
-    northeast = field[row - 1][col + 1];
-    northwest = field[row - 1][col - 1];
+  createFieldWithMines(numOfRows, numOfCols, numOfMines) {
+    const mines = Array(numOfMines).fill('M');
+    const field = Array(numOfRows * numOfCols - numOfMines)
+      .fill(0)
+      .concat(mines);
+    return this.shuffle(field);
   }
 
-  if (field[row + 1]) {
-    southeast = field[row + 1][col + 1];
-    south = field[row + 1][col];
-    southwest = field[row + 1][col - 1];
-  }
-
-  west = field[row][col - 1];
-  east = field[row][col + 1];
-
-  [east, west, north, south, northeast, northwest, southeast, southwest].forEach(spread);
-}
-
-function makeMinesField(x, y, numberOfMines) {
-  var flattenMinesField = makeFlattenMinesField(x, y, numberOfMines);
-  var field = [];
-
-  for (var i = 0; i < x; i++) {
-    var row = [];
-    for (var j = 0; j < y; j++) {
-      var cell = flattenMinesField[i * y + j];
-      row.push(cell);
-    }
-    field.push(row);
-  }
-
-  return setMarkersOnField(field);;
-}
-
-function makeFlattenMinesField(x, y, numOfMines) {
-  var flattenMinesField = new Array(x * y);
-  for (var i = 0; i < numOfMines; i++) {
-    flattenMinesField[i] = makeCell(true);
-  }
-
-  return shuffle(flattenMinesField);
-
-  function shuffle(array) {
+  shuffle(array) {
     var len = array.length;
     array.forEach(function(n, i) {
       var rand = Math.floor(Math.random() * len);
-      swap(array, i, rand);
+      var tmp = array[i];
+      array[i] = array[rand];
+      array[rand] = tmp;
     });
     return array;
   }
 
-  function swap(array, x, y) {
-    var tmp = array[x];
-    array[x] = array[y];
-    array[y] = tmp;
-  }
-
-}
-
-function setMarkersOnField(field) {
-  field.forEach(function(row, i) {
-    row.forEach(function(cell, j) {
-      var totalSurroundingBombs = findSurroundingBombsAtCell(i, j, field);
-      row[j] = totalSurroundingBombs;
-      row[j].row = i;
-      row[j].col = j;
+  addNumberIndicatorsToMines(field) {
+    return field.map((cell, i) => {
+      return cell === 'M' ? 'M' :
+        this.getSurroundIndex(i, field)
+          .map(i => field[i])
+          .reduce((sum, n) => n === 'M' ? sum + 1 : sum, 0)
     });
-  });
+  }
 
-  return field;
+  getSurroundIndex(i) {
+    let indices = [];
+    const rightEdge = !((i + 1) % this.col);
+    const leftEdge = !(i % this.col);
+    const upEdge = i < this.col;
+    const downEdge = i >= this.col * (this.row - 1);
 
-  function findSurroundingBombsAtCell(row, col, field) {
-    var cell = field[row][col] || this.makeCell();
+    return [
+      !rightEdge && i + 1,
+      !downEdge && i + this.row,
+      !rightEdge && !downEdge && i + 1 + this.row,
+      !rightEdge && !upEdge && i + 1 - this.row,
+      !leftEdge && i - 1,
+      !upEdge && i - this.row,
+      !leftEdge && !upEdge && i - 1 - this.row,
+      !leftEdge && !downEdge && i - 1 + this.row
+    ];
+  }
 
-    var east, west, north, south, northeast, northwest, southeast, southwest;
-
-    if (cell.isMine) {
-      cell.value = null;
-    } else {
-      if (field[row - 1]) {
-        north = field[row - 1][col];
-        northeast = field[row - 1][col + 1];
-        northwest = field[row - 1][col - 1];
-      }
-
-      if (field[row + 1]) {
-        southeast = field[row + 1][col + 1];
-        south = field[row + 1][col];
-        southwest = field[row + 1][col - 1];
-      }
-
-      west = field[row][col - 1];
-      east = field[row][col + 1];
-
-      cell.value = [east, west, north, south, northeast, northwest, southeast, southwest]
-        .reduce(function(acc, n) {
-          return acc + (n && n.isMine ? 1 : 0);
-        }, 0);
+  toggle(i) {
+    switch(true) {
+      case (this.isOpen[i]):
+      case (this.isFlag[i]):
+        break;
+      case (this.field[i] > 0):
+      case (this.field[i] === 'M'):
+        this.isOpen[i] = true;
+        break;
+      case (this.field[i] === 0):
+        this.isOpen[i] = true;
+        this.getSurroundIndex(i)
+          .forEach(cell => this.toggle(cell));
+      default:
+        break;
     }
+  }
 
-    return cell;
+  flag(i) {
+    switch(true) {
+      case (this.isOpen[i]):
+        break;
+      default:
+        const flag = this.isFlag[i];
+        this.isFlag[i] = !flag;
+        break;
+    }
   }
 }
 
-function makeCell(isMine) {
-  return {
-    isMine: isMine,
-    isOpen: false,
-    value: null
-  };
+/**
+  Minesweeper View Class
+*/
+class MinesweeperView {
+  constructor(numOfRows, numOfCols) {
+    this.row = numOfRows;
+    this.col = numOfCols;
+    this.onClick = null;
+    this.rootElement = document.createElement('div');
+  }
+
+  render(field, opens, flags) {
+    this.rootElement.innerHTML = '';
+
+    for(let i = 0; i < this.row; i++) {
+      const rowElement = document.createElement('div');
+      rowElement.className = 'minesweeper-row';
+        
+      for(let j = 0; j < this.col; j++) {
+        const cellIndex = i * this.row + j;
+        const cellElement = document.createElement('button');
+
+        cellElement.className = this.makeClassName(cellIndex, field, opens, flags);
+        cellElement.addEventListener('click', e => this.onClick(e, cellIndex));
+        cellElement.addEventListener('contextmenu', e => {
+          e.preventDefault();
+          this.onRightClick(e, cellIndex);
+        });
+
+        rowElement.appendChild(cellElement);
+      }
+
+      this.rootElement.appendChild(rowElement);
+    }
+  }
+
+  update(field, opens, flags) {
+    for(let i = 0; i < this.row; i++) {
+      const rowElement = this.rootElement.childNodes[i];
+      for(let j = 0; j < this.col; j++) {
+        const cellIndex = i * this.row + j;
+        const cellElement = rowElement.childNodes[j];
+        cellElement.className = this.makeClassName(cellIndex, field, opens, flags);
+      }
+    }
+  }
+
+  makeClassName(i, field, opens, flags) {
+    switch(true) {
+      case flags[i]:
+        return 'minesweeper-cell minesweeper-cell__flagged';
+      case opens[i]:
+        return `minesweeper-cell minesweeper-cell__clicked minesweeper-cell--${field[i]}`;
+      default:
+        return 'minesweeper-cell';
+    }
+  }
 }
+
+/**
+  Minesweeper Controller Class
+*/
+class Minesweeper {
+  constructor(x, y, n) {
+    const minesweeper = new MinesweeperModel(10, 10, 20);
+    const view = new MinesweeperView(10, 10);
+    view.render(minesweeper.field, minesweeper.isOpen, minesweeper.isFlag);
+    view.onClick = (e, i) => {
+      minesweeper.toggle(i);
+      view.update(minesweeper.field, minesweeper.isOpen, minesweeper.isFlag);
+    };
+
+    view.onRightClick = (e, i) => {
+      minesweeper.flag(i);
+      view.update(minesweeper.field, minesweeper.isOpen, minesweeper.isFlag);
+    }
+    this.el = view.rootElement;
+    this.el.className = 'minesweeper';
+  }
+}
+
+// var root = document.querySelector('#root');
+
+// function make() {
+//   root.appendChild(new Minesweeper(10, 10, 20).el);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
